f0110d3add45d0632c4025f0fd61537bf2cbf23b
Ronald <ronald.cristobal.lemus@outlook.com>
2021-09-11 19:23:28 -0400

    Final Commit

--- src/Computing_Simple_Interest.java
+++ src/Computing_Simple_Interest.java
@@ -1,5 +1,29 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1.12 Solution
+ *  Copyright 2021 Ronald Lemus
+ */
+
+
+import java.util.Scanner;
+
 public class Computing_Simple_Interest {
     public static void main(String[] arg){
-        
+        System.out.print("Enter the principal: ");
+        Scanner principalInp = new Scanner(System.in);
+        int principal = principalInp.nextInt();
+
+        System.out.print("Enter the percentage rate of interest: ");
+        Scanner rateInp = new Scanner(System.in);
+        double rate = rateInp.nextDouble();
+        double percentage = rate/100;
+
+        System.out.print("Enter the number of years: ");
+        Scanner yearsInp = new Scanner(System.in);
+        int years = yearsInp.nextInt();
+
+        double total = principal + (principal * percentage * years);
+
+        System.out.printf("After %d years at %.2f%%, the investment will be worth $%.2f", years, rate,total);
+
     }
 }
